f80d3827f3c4ae188ef57ed76d151b4724f50f76
ehfaz <ehfaz@ferntechsolutions.com>
2019-12-13 03:06:41 +0600

    Added grid to workspace

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,6 +3,20 @@ let number
 
 const grid = 8
 const backgroundColor = '#f8f8f8'
+const lineStroke = '#ebebeb'
+const tableFill = 'rgba(255,0, 0, 0.4)'
+const tableStroke = 'rgba(255,0, 0, 0.5)'
+const tableShadow = 'rgba(0, 0, 0, 0.4) 3px 3px 7px'
+const chairFill = 'rgba(67, 42, 4, 0.7)'
+const chairStroke = '#32230b'
+const chairShadow = 'rgba(0, 0, 0, 0.4) 3px 3px 7px'
+const barFill = 'rgba(0, 93, 127, 0.7)'
+const barStroke = '#003e54'
+const barShadow = 'rgba(0, 0, 0, 0.4) 3px 3px 7px'
+const barText = 'Bar'
+const wallFill = 'rgba(136, 136, 136, 0.7)'
+const wallStroke = '#686868'
+const wallShadow = 'rgba(0, 0, 0, 0.4) 5px 5px 20px'
 
 let widthEl = document.getElementById('width')
 let heightEl = document.getElementById('height')
@@ -49,6 +63,29 @@ function initCanvas() {
   number = 1
   canvas.backgroundColor = backgroundColor
 
+  // Load x-axis
+  for (let i = 0; i < (canvas.height / grid); i++) {
+    const lineX = new fabric.Line([0, i * grid, canvas.height, i * grid], {
+      stroke: lineStroke,
+      selectable: false,
+      type: 'line'
+    })
+    sendLinesToBack()
+    canvas.add(lineX)
+  }
+
+  // Load y-axis
+  for (let i = 0; i < (canvas.width / grid); i++) {
+    const lineY = new fabric.Line([i * grid, 0, i * grid, canvas.height], {
+      stroke: lineStroke,
+      selectable: false,
+      type: 'line'
+    })
+    sendLinesToBack()
+    canvas.add(lineY)
+  }
+
+  
   canvas.on('object:moving', function (options) {
     options.target.set({
       left: Math.round(options.target.left / grid) * grid,
@@ -65,5 +102,78 @@ function initCanvas() {
     checkBoudningBox(e)
   })
 }
+
+// Code to snap items to grid
+function snapToGrid(target) {
+  target.set({
+    left: Math.round(target.left / (grid / 2)) * grid / 2,
+    top: Math.round(target.top / (grid / 2)) * grid / 2
+  })
+}
+
+// Check bounding box
+function checkBoundingBox(e) {
+  const obj = e.target
+
+  if (!obj) {
+    return
+  }
+  obj.setCoords()
+
+  const objBoundingBox = obj.getBoundingRect()
+  if (objBoundingBox.top < 0) {
+    obj.set('top', 0)
+    obj.setCoords()
+  }
+  if (objBoundingBox.left > canvas.width - objBoundingBox.width) {
+    obj.set('left', canvas.width - objBoundingBox.width)
+    obj.setCoords()
+  }
+  if (objBoundingBox.top > canvas.height - objBoundingBox.height) {
+    obj.set('top', canvas.height - objBoundingBox.height)
+    obj.setCoords()
+  }
+  if (objBoundingBox.left < 0) {
+    obj.set('left', 0)
+    obj.setCoords()
+  }
+}
+
+// Send to back
+function sendLinesToBack() {
+  canvas.getObjects().map(o => {
+    if (o.type === 'line') {
+      canvas.sendToBack(o)
+    }
+  })
+}
+
+// Set active rectangle
+// document.querySelectorAll('.rectangle')[0].addEventListener('click', function () {
+//   const o = addRect(0, 0, 60, 60)
+//   canvas.setActiveObject(o)
+// })
+
+// Remove active object
+// document.querySelectorAll('.remove')[0].addEventListener('click', function () {
+//   const o = canvas.getActiveObject()
+//   if (o) {
+//     o.remove()
+//     canvas.remove(o)
+//     canvas.discardActiveObject()
+//     canvas.renderAll()
+//   }
+// })
+
+// Add circle
+// $("#addCircle").click(function () {
+//   canvas.add(new fabric.Circle({
+//     radius: 50,
+//     left: 0,
+//     top: 0,
+//     fill: '#0B61A4'
+//   }));
+// });
+
 
 initCanvas();
